c8e4ff630670d062f22c837e749c23887d9a5545
Barkito <ian.benelramos.1@gmail.com>
2023-02-10 11:16:47 -0500

    Creacion de controlador

---
+++ PRUEBA1/Controllers/PruebaHomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PRUEBA1.Controllers
+{
+    public class PruebaHomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
